1696017e965dedd86cba716745af1eddc3e02c9e
Kevin Matthews <soloha@gmail.com>
2026-01-14 21:04:18 -0500

    Make logo transparent

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,9 +6,11 @@
 
 <div>
 	<div>
-		<div style="display: flex; justify-content: center">
-			<img src="/logo.jfif" style="aspect-ratio: 747/1024; height: 250px"/>
+		<div style="display: flex; justify-content: center; margin: 20px 0">
+			<img src="/logo.png" alt="logo" style="aspect-ratio: 1392/876; height: 150px"/>
 		</div>
+		<!--<div style="transform: rotate(90deg); position: fixed; right: -50%; background: black; width:100%; padding:25px 0 5px 0px">
+			<div style="display: flex; justify-content: space-between; margin: 0 auto; width: 300px; font-size: 1.3rem">-->
 		<div>
 			<div style="display: flex; justify-content: space-between; margin: 0 auto; width: 300px; font-size: 1.5rem">
 				<a href="/home">Home</a>
