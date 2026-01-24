cd0887c6c3a04774780aba9330fe435ffb52e0af
Kevin Matthews <solox174@users.noreply.github.com>
2026-01-23 19:56:12 -0500

    Add Amplify (#5)
    * Delete .github/workflows/codeql.yml
    
    * Add active page indicator in nav
    
    * Add active page indicator in nav
    
    * Add Amplify configuration to app

---
+++ PAIRING.md
@@ -0,0 +1,23 @@
+### Session #1:
+TODO:
+1. install [Node](https://nodejs.org/dist/v24.13.0/node-v24.13.0-x64.msi)
+2. in console
+  - `npm i`
+ 
+Concepts:
+- truthy
+- data models
+- abstraction
+- encapsulation
+
+### Session #2:
+TODO: 
+1. install [aws cli](https://awscli.amazonaws.com/AWSCLIV2.msi)
+
+Concepts:
+- three tier application architecture
+  - view
+  - business logic
+  - data access 
+  - singleton
+- the difference between JavaScript and TypeScript
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,11 +10,8 @@
 		<div style="display: flex; justify-content: center; margin: 20px 0">
 			<img src="/logo.png" alt="logo" style="aspect-ratio: 1392/876; height: 150px"/>
 		</div>
-		<!--<div style="transform: rotate(90deg); position: fixed; right: -50%; background: black; width:100%; padding:25px 0 5px 0px">
-			<div style="display: flex; justify-content: space-between; margin: 0 auto; width: 300px; font-size: 1.3rem">-->
 		<div>
 			<div style="display: flex; justify-content: space-between; margin: 0 auto; width: 400px; font-size: 1.5rem">
-				<!-- talk about "truthy" -->
 				<a class:active-nav="{page.url.pathname === '/'}" href="/">Home</a>
 				<a class:active-nav="{page.url.pathname === '/scheduling'}" href="/scheduling">Scheduling</a>
 				<a class:active-nav="{page.url.pathname === '/services'}" href="/services">Services</a>
