efd6d7534b6059f7b58378f0a73cd512b02ab8ca
Kevin Matthews <soloha@gmail.com>
2026-05-07 21:50:14 -0400

    Enhance mobile view

--- src/lib/component/CreateAccount.svelte
+++ src/lib/component/CreateAccount.svelte
@@ -1,38 +1,36 @@
 <script>
 </script>
 
-
 <div style="display: flex; justify-content: center; margin: 0 auto;">
-    <form method="post">
+    <form method="post" style="width: min(400px, 100%)">
         <legend class="section-title">Create Account</legend>
-        <div style="display: flex; justify-content: space-between; width: min(400px, 100%); margin: 0 auto;">
+        <div style="display: flex; justify-content: space-between; width: 100%;">
             <fieldset
-                    style="display: flex; justify-content: space-between; border: none;  width: 100px; padding: 0; flex-direction: column; row-gap: 20px; ">
-                <div>
+                    style="display: flex; border: none; width: auto; padding: 0; flex-direction: column; row-gap: 20px;">
+                <div style="display: flex; flex-direction: column">
                     <label for="first-name">First Name:</label>
                     <input id="first-name" name="last-name" type="text" required/>
                 </div>
-                <div>
+                <div style="display: flex; flex-direction: column">
                     <label for="email">Email:</label>
                     <input id="email-address" name="email" type="email" required/>
                 </div>
-                <div>
+                <div style="display: flex; flex-direction: column">
                     <label for="password">Password:</label>
                     <input id="password" name="password" type="password" required/>
                 </div>
-
             </fieldset>
             <fieldset
-                    style="display: flex; justify-content: space-between; border: none; width: 100px; padding: 0; flex-direction: column; row-gap: 20px;">
-                <div>
+                    style="display: flex; border: none; width: auto; padding: 0; flex-direction: column; row-gap: 20px;">
+                <div style="display: flex; flex-direction: column">
                     <label for="last-name">Last Name:</label>
                     <input id="last-name" name="last-name" type="text" required/>
                 </div>
-                <div>
+                <div style="display: flex; flex-direction: column">
                     <label for="phone-number">Phone Number:</label>
                     <input id="phone-number" name="phone-number" type="number" required/>
                 </div>
-                <div>
+                <div style="display: flex; flex-direction: column">
                     <label for="confirm-password">Confirm Password:</label>
                     <input id="confirm-password" name="confirm-password" type="password" required/>
                 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,11 @@
+<script>
+</script>
+
 <svelte:head>
     <title>Home</title>
 </svelte:head>
 
 <svelte:window onkeydown={onKeydown} />
 
-<a href="/portfolio">See my work</a>
+<div style="padding-bottom: 10px">A few examples of the finishes we deliver across a range of vehicle conditions.</div>
+<a href="/portfolio" style="font-size: 1.2em">Results</a>
