59c5703f3f75e66021db47d615da8f9b0c58abcb
Kevin Matthews <soloha@gmail.com>
2026-05-11 15:41:56 -0400

    Update TODO specifying email as appointment contact method

--- src/routes/scheduling/+page.server.ts
+++ src/routes/scheduling/+page.server.ts
@@ -1,6 +1,6 @@
 import type { Actions } from './$types';
 
-// TODO: use `@aws-sdk/client-sns` to send SMS with appointment details to Jay
+// TODO: use `@aws-sdk/client-ses` to send email with appointment details to Jay and confirmation email to customer
 // TODO: Add UI feedback to customer on error
 export const actions = {
     default: async ({request}) => {
