74c2eb357bea76783c9664ecdd5925c894b584eb
Kevin Matthews <soloha@gmail.com>
2026-05-11 15:47:34 -0400

    Add TODO for appointment confirmation

--- src/routes/scheduling/+page.server.ts
+++ src/routes/scheduling/+page.server.ts
@@ -2,6 +2,7 @@ import type { Actions } from './$types';
 
 // TODO: use `@aws-sdk/client-ses` to send email with appointment details to Jay and confirmation email to customer
 // TODO: Add UI feedback to customer on error
+// TODO:  ADD appointment confirmation message to confirm appointment "Your appointment is confirmed for May 20th at 10AM"
 export const actions = {
     default: async ({request}) => {
         let x = await request.formData()
