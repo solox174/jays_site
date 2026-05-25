550258dc6418886f89e7dbd667e31b18994d2b2f
Coding-Sloth300 <codingsloth@outlook.com>
2026-05-25 13:00:47 -0700

    add confirm account page (#6)

---
+++ src/routes/confirm-account/+page.svelte
@@ -0,0 +1,16 @@
+```
+<script>
+</script>
+
+<title>Confirm Account</title>
+<div class="glass-panel">
+    <form method="post" style="max-width: 400px; padding: 20px; margin: 0 auto; text-align: justify">
+        <h1>Confirm Your Email</h1>
+        <p>Thank you for signing up with Jay's Auto Detailing! Almost there — just one more step before we let you in! We've sent a confirmation code to your email. Please enter it below to verify your account.</p>
+        <label for="confirmation-code" style="text-align: center; display: block">Confirmation Code:</label>
+        <div style="display: flex; flex-direction: column; align-items: center; gap: 10px; margin-top: 10px">
+            <input id="confirmation-code" name="verify-account" required type="text"/>
+            <button type="submit">Verify Code</button>
+        </div>
+    </form>
+</div>
