69b51da322c1bd41480290c0066ce50b1ccd3b21
Christopher Hiller <codingsloth@outlook.com>
2026-06-03 12:09:36 -0700

    fix email type in SessionUser and verifySession

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,7 +1,7 @@
 declare global {
     namespace App {
         interface Locals {
-            user: { id: string; email?: string } | null;
+            user: { id: string; email: string } | null;
             token?: string;
         }
     }
